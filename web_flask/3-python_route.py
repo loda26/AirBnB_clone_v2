04bc5109f8a54046613c1fb6e030438ab816f66a
loda26 <mokhaled26498@gmail.com>
2024-03-21 23:01:33 +0200

    task

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,5 +18,12 @@ def c_text(text):
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
