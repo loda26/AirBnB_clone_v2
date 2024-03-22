5db82a1d1c6298c7ad0c53a8b43440cd026651ca
loda26 <mokhaled26498@gmail.com>
2024-03-23 01:47:10 +0200

    task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
-
+"""A script that starts a Flask web application:
+listening on 0.0.0.0, port 5000"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
+def hello_bnb():
+    """Starts a Flask application"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Starts a Flask application"""
     return 'HBNB'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
